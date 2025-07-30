40277ae5196b9228481b2e3dce71ba634c4eef26
eugeneleejunping <eugeneleejunping@gmail.com>
2025-07-30 23:38:24 +0800

    changed file path

--- src/core/urls.py
+++ src/core/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('files.urls')),
+    path('api/', include('src.files.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
